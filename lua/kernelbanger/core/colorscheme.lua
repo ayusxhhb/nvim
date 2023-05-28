d8e7d4da7ebe28d101b31a909bfc658eb5968f37
Ayush <its2003ayush@gmail.com>
2023-05-28 20:46:27 +0530

    just small changes

--- lua/kernelbanger/core/colorscheme.lua
+++ lua/kernelbanger/core/colorscheme.lua
@@ -1,7 +1,7 @@
--- set colorscheme to nightfly with protected call
+-- set colorscheme to tokyonight-night with protected call
 -- in case it isn't installed
 local status, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
 if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
+	print("Colorscheme not found!") -- print error if colorscheme not installed
+	return
 end
